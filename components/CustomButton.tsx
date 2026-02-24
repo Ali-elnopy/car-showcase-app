7b3e29b4da9778a8a84e40c07341f8f707de2c12
Ali <utfcystgvytf@gmail.com>
2026-02-24 11:17:44 +0200

    add NavBar, Hero, and Footer Sections

---
+++ components/CustomButton.tsx
@@ -0,0 +1,19 @@
+"use client";
+import { CustomButtonProps } from "@/types";
+import Image from "next/image";
+
+
+const CustomButton = ({title, containerStyles, handelClick, btnType}: CustomButtonProps) => {
+    return (
+    <button
+        disabled={false}
+        type={btnType || "button"}
+        className={`custom-btn ${containerStyles}`}
+        onClick={()=>{handelClick}}
+        >
+            <span className={`flex-1 `}>{title}</span>
+        </button>
+    )
+}
+
+export default CustomButton
---
+++ types/index.ts
@@ -0,0 +1,9 @@
+import { MouseEventHandler } from "react";
+export interface CustomButtonProps{
+    title: string;
+    containerStyles?: string;
+    handelClick?: 
+    MouseEventHandler<HTMLButtonElement>;
+    btnType?: "button" | "submit";
+
+}
